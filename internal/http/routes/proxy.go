61bd82b1d8c44566e0808619031c2a8532debb19
Shindou Mihou <git@mihou.pw>
2024-11-01 09:51:57 +0800

    fix: proxy not properly working

--- internal/http/routes/proxy.go
+++ internal/http/routes/proxy.go
@@ -2,6 +2,7 @@ package routes
 
 import (
 	"amelia-sh-proxy/internal/http"
+	"amelia-sh-proxy/internal/responses"
 	"github.com/gofiber/fiber/v2"
 	"github.com/imroc/req/v3"
 )
@@ -10,7 +11,13 @@ var RegularClient = req.C().SetBaseURL("https://scribblehub.com/")
 
 var _ = http.Fiber.Attach(func(app *fiber.App) {
 	app.Get("/*", func(c *fiber.Ctx) error {
-		resp := RegularClient.Get(c.Params("*")).
+		responses.Logger(c).Info().
+			Str("path", c.Path()).
+			Any("queries", c.Queries()).
+			Str("request-id", c.GetRespHeader("X-Request-Id")).
+			Msg("Proxying request")
+		resp := RegularClient.Get(c.Path()).
+			SetQueryParams(c.Queries()).
 			SetHeader("User-Agent", "Amelia/2.1 (Language=Kotlin/1.7.10, Developer=Shindou Mihou)").
 			Do()
 		if resp.Err != nil {
